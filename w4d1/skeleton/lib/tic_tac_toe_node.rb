a01fa2d7a30faa43d0643219125d72a0863abf41
caroger <rogerhu1989@gmail.com>
2020-12-07 14:24:30 -0800

    feat: children in progress

--- w4d1/skeleton/lib/tic_tac_toe_node.rb
+++ w4d1/skeleton/lib/tic_tac_toe_node.rb
@@ -16,24 +16,24 @@ class TicTacToeNode
   def winning_node?(evaluator)
   end
 
+  def flip_mark
+    @next_mover_mark == :x ? :o : :x
+  end
+
   # This method generates an array of all moves that can be made after
   # the current move.
   def children
     poss_moves = [] 
     (0..2).each do |row|
       (0..2).each do |col|
-
-        #1) check if empty 
-        #2) create @board.dup and place opposite mark at that position if empty
-
-        # p next_mover_mark
-        # p @board[row,col]
-        # cycle through these positions, and see if there's already a mark,
-        #if not, place it in poss_moves
-        #  child_node = TicTacToeNode.new(board,next_mover_mark) 
-        p @board
-        # poss_moves << [row,col] if @prev_move_pos != [row,col]
-        # not sure if here we have to pull up the board and get ALL previous moves here
+        pos = [row, col]
+        if @board.empty?(pos)
+          new_node = @board.dup
+          new_node[pos] = @next_mover_mark
+          # new_node.next_mover_mark = flip_mark
+          # new_node.prev_move_pos =  pos
+          # poss_moves << new_node
+        end
       end
     end
     poss_moves
