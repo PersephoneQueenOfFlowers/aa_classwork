7200807e7ace2d9fcd346fe2d7569c71c52d4079
Mona Miga <74436129+mona-miga@users.noreply.github.com>
2020-12-08 14:11:19 -0800

    Initial code for employee-manager class

--- w4d2/employee_manager.rb
+++ w4d2/employee_manager.rb
@@ -1,11 +1,28 @@
 class Employee 
 
+  attr_reader :name, :title, :salary, :boss
+  def initialize(name, title, salary, boss)
+    @name = name
+    @title = title
+    @salary = salary
+    @boss = boss
+  end
+
+  def bonus(mult)
+    bonus = @salary * mult
+  end
 
 
 end
 
 
-class Manager 
+class Manager < Employee
 
+  def initialize(name, title, salary, boss, employees)
+    super(name, title, salary, boss)
+    @employees = employees # array of all empl and sub-empl
+  end
 
 end
+
+emp_1 = Employee.new("Ned", "IT Consultant", 70000, mgr_1)
