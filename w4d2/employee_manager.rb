bf6ebc2bfc9ef2d14200a5ca3cdb2582b8f421de
seth_schoenfeld <sethschoenfeld@gmail.com>
2020-12-08 15:17:08 -0800

    calculate emp bonus from salaries

--- w4d2/employee_manager.rb
+++ w4d2/employee_manager.rb
@@ -20,26 +20,35 @@ end
 
 
 class Manager < Employee
-
+  attr_reader :employees
+  attr_accessor :emp_salaries
   def initialize(name, title, salary, employees)
     super(name, title, salary)
     @employees = employees # array of all empl and sub-empl
+    @emp_salaries = 0 
+    emp_salaries
   end
 
   def emp_salaries
-    sal = @employees.inject { |acc, emp| acc + emp.salary }
+    @employees.each do |emp| 
+      @emp_salaries += emp.salary
+    end
+    p @emp_salaries
   end
 
   def bonus(mult)
-    # @bonus = sal_arr.each * multiplier 
+    @bonus = @emp_salaries * mult 
   end
 end
 
-p emp_1 = Employee.new("Ned", "IT Consultant", 70000)
-p emp_1.boss("Nicole")
+emp_1 = Employee.new("Ned", "IT Consultant", 70000)
+emp_2 = Employee.new("Tom", "software engineer", 80000)            
+emp_1.boss("Nicole")
+emp_2.boss("Nicole")
 
-p mgr_1 = Manager.new("Nicole", "Team Leader", 100000, [emp_1])
-
+mgr_1 = Manager.new("Nicole", "Team Leader", 100000, [emp_1,emp_2])
+p mgr_1.bonus(0.5)
+# p mgr_1.emp_salaries
 
 # def dfs salary
 #     return nil if self.nil?
