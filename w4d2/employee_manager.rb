74d16769b4778fe56540d52ad4cd6bc400908587
Mona Miga <74436129+mona-miga@users.noreply.github.com>
2020-12-08 14:53:08 -0800

    add emp-salaries method and def manager

--- w4d2/employee_manager.rb
+++ w4d2/employee_manager.rb
@@ -21,21 +21,31 @@ end
 
 class Manager < Employee
 
-  def initialize(name, title, salary, boss, employees)
-    super(name, title, salary, boss)
+  def initialize(name, title, salary, employees)
+    super(name, title, salary)
     @employees = employees # array of all empl and sub-empl
   end
 
+  def emp_salaries
+    sal = @employees.inject { |acc, emp| acc + emp.salary }
+  end
+
+  def bonus(mult)
+    # @bonus = sal_arr.each * multiplier 
+  end
 end
 
 p emp_1 = Employee.new("Ned", "IT Consultant", 70000)
 p emp_1.boss("Nicole")
 
-# def dfs target
+p mgr_1 = Manager.new("Nicole", "Team Leader", 100000, [emp_1])
+
+
+# def dfs salary
 #     return nil if self.nil?
-#     return self if self.value == target
-#     @children.each do |child|
-#       result = child.dfs(target)
+#     return self if self.value == salary
+#     @employees.each do |emp|
+#       result = emp.dfs(salary)
 #       return result unless result.nil?
 #     end
 #     nil
