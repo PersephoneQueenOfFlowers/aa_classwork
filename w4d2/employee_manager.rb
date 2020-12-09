b674fe984b91725656c66866034fa5e444cdb6e5
Mona Miga <74436129+mona-miga@users.noreply.github.com>
2020-12-08 16:23:30 -0800

    add dfs method for salaries

--- w4d2/employee_manager.rb
+++ w4d2/employee_manager.rb
@@ -25,37 +25,48 @@ class Manager < Employee
   def initialize(name, title, salary, employees)
     super(name, title, salary)
     @employees = employees # array of all empl and sub-empl
-    @emp_salaries = 0 
-    emp_salaries
+    @emp_salaries = emp_salaries
   end
 
   def emp_salaries
-    @employees.each do |emp| 
-      @emp_salaries += emp.salary
-    end
-    p @emp_salaries
+    self.dfs
+    # @employees.each do |emp| 
+    #   @emp_salaries += emp.salary
+    # end
+    # p @emp_salaries
   end
 
   def bonus(mult)
     @bonus = @emp_salaries * mult 
   end
+
+  def dfs
+    return nil if self.employees.empty?
+    @emp_salaries
+    @employees.each do |emp|
+      @emp_salaries += emp.salary += emp.dfs
+    end
+    nil
+  end
+
 end
 
 emp_1 = Employee.new("Ned", "IT Consultant", 70000)
-emp_2 = Employee.new("Tom", "software engineer", 80000)            
-emp_1.boss("Nicole")
-emp_2.boss("Nicole")
+emp_2 = Employee.new("Tom", "software engineer", 80000) 
+emp_4 = Employee.new("Tim", "janitor", 45000)           
+emp_5 = Employee.new("Dan", "janitor", 45000)
 
 mgr_1 = Manager.new("Nicole", "Team Leader", 100000, [emp_1,emp_2])
-p mgr_1.bonus(0.5)
-# p mgr_1.emp_salaries
+emp_3 = Manager.new("Joe", "plant services manager", 60000, [emp_4, emp_5])
 
-# def dfs salary
-#     return nil if self.nil?
-#     return self if self.value == salary
-#     @employees.each do |emp|
-#       result = emp.dfs(salary)
-#       return result unless result.nil?
-#     end
-#     nil
-#   end
+
+
+emp_1.boss(mgr_1)
+emp_2.boss(mgr_1)
+emp_3.boss(mgr_1)
+emp_4.boss(emp_3)
+emp_5.boss(emp_3)
+
+# p mgr_1.bonus(0.5)
+p mgr_1.emp_salaries
+
