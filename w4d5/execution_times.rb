8361614c2a13be8dbcae215cfaeef4028926f4ad
seth_schoenfeld <sethschoenfeld@gmail.com>
2020-12-11 11:59:29 -0800

    phase 1 contiguous subsets

--- w4d5/execution_times.rb
+++ w4d5/execution_times.rb
@@ -4,5 +4,20 @@ def my_min(array) # Phase I: O(n^2) | Phase II: O(n)
   min
 end
 
+def largest_contiguous_subsum(arr) #Phase 1 solution O(2^n) or O(n^2)??
+
+  subs = [] 
+  (0...arr.length).each do |i1|  
+    (0...arr.length).each do |i2|
+      subs << arr[i1..i2] if i2 > i1       
+    end
+  end
+  max = subs[0].sum 
+  subs.each do |sub_array|
+    max = sub_array.sum if sub_array.sum > max 
+  end
+  max
+end
+
 
 
