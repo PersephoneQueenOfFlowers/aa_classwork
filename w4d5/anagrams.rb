386ff6c0804ecf7090b10f2db66034baff9047ce
seth_schoenfeld <sethschoenfeld@gmail.com>
2020-12-11 16:38:01 -0800

    solved 2nd stage anagrams

--- w4d5/anagrams.rb
+++ w4d5/anagrams.rb
@@ -1,13 +1,24 @@
-
-def first_anagram(str1, str2)
-    anas = []
-    str_length = str2.length 
-# str1 = "abac" str2 = "cbaa"
-    perms = str1.permutation.to_a
-    p perms
+require 'byebug'
+def first_anagram(str1, str2) # Big O = O(N!) where n is string length
+  perms = str1.split("").permutation.to_a
+  perms.include?(str2.split(""))
 end
 
-
 str1 = "abac" 
-str2 = "cbaa"
-p first_anagram(str1, str2)
+str2 = "cbar"
+# p first_anagram(str1, str2)
+
+def second_anagram?(str1, str2)
+  # debugger
+  str1.each_char do |char1|
+    i = str2.index(char1)
+    if i== nil
+      return false  
+    else   
+      str2 = str2[0...i] + str2[i+1..-1]
+    end
+  end
+  str2.empty? 
+end
+
+p second_anagram?(str1, str2)
