32d952029cf6192b3bafda17015e54343107df70
Brynn <brynnreginajohnson@gmail.com>
2020-12-11 18:56:15 -0600

    Finish anagrams

--- w4d5/anagrams.rb
+++ w4d5/anagrams.rb
@@ -8,8 +8,7 @@ str1 = "abac"
 str2 = "cbar"
 # p first_anagram(str1, str2)
 
-def second_anagram?(str1, str2)
-  # debugger
+def second_anagram?(str1, str2) #O(n^2) ?? 
   str1.each_char do |char1|
     i = str2.index(char1)
     if i== nil
@@ -21,4 +20,21 @@ def second_anagram?(str1, str2)
   str2.empty? 
 end
 
-p second_anagram?(str1, str2)
+# p second_anagram?(str1, str2)
+
+def third_anagram?(str1, str2) #O(n logn)
+  str1 = str1.split("").sort.join
+  str2 = str2.split("").sort.join 
+  str1 == str2
+end
+
+#p third_anagram?(str1, str2)
+
+def fourth_anagram?(str1, str2) #O(n)
+  str_hash_1 = Hash.new(0)
+  str_hash_2 = Hash.new(0)
+  str1.each_char {|char| str_hash_1[char] += 1 }
+  str2.each_char {|char| str_hash_2[char] += 1 }
+  str_hash_1 == str_hash_2
+end
+p fourth_anagram?(str1, str2)
