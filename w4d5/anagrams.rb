f24603c3e305a62ff881ed64d3cb830e28b64034
Brynn <brynnreginajohnson@gmail.com>
2020-12-11 18:03:18 -0600

    Start first_anagrams

---
+++ w4d5/anagrams.rb
@@ -0,0 +1,13 @@
+
+def first_anagram(str1, str2)
+    anas = []
+    str_length = str2.length 
+# str1 = "abac" str2 = "cbaa"
+    perms = str1.permutation.to_a
+    p perms
+end
+
+
+str1 = "abac" 
+str2 = "cbaa"
+p first_anagram(str1, str2)
