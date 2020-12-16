7e619afb658cf0c569396abae30cf1dcd3bf6573
appkenny <kzeng.95@gmail.com>
2020-12-15 16:22:48 -0800

    Stuck on 04 spares_continents

--- w5d2/sql_zoo/lib/04_select_within_select.rb
+++ w5d2/sql_zoo/lib/04_select_within_select.rb
@@ -97,5 +97,13 @@ def sparse_continents
   # population.
   # Hint: Sometimes rewording the problem can help you see the solution.
   execute(<<-SQL)
+  SELECT name, continent, population 
+  FROM countries
+  WHERE  IN 
+  (
+    SELECT DISTINCT continent
+    FROM countries
+    WHERE population < 25000000
+  );
   SQL
 end
