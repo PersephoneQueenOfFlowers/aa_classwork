6f4b6ceaf7ae60bd43c44e82fda3f53b34c06749
your name here <rupesh_singh87@yahoo.com>
2020-12-17 17:10:44 -0600

    finished enrolment and user association part

--- w5d4/associations/app/models/user.rb
+++ w5d4/associations/app/models/user.rb
@@ -1,2 +1,17 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :bigint           not null, primary key
+#  name       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class User < ApplicationRecord
+
+    belongs_to :enrolled,
+        primary_key: :id,
+        foreign_key: :course_id,
+        class_name: :Enrollment
+
 end
