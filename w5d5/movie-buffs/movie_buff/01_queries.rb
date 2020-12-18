65cc3c6f3fb916d79b8d21e2dc61f908f084a8a1
Gregory-DelCarlo <ethandelcarlo@gmail.com>
2020-12-18 14:17:50 -0800

    Worked on biggest_cast query

--- w5d5/movie-buffs/movie_buff/01_queries.rb
+++ w5d5/movie-buffs/movie_buff/01_queries.rb
@@ -41,6 +41,8 @@ def biggest_cast
   # Find the id and title of the 3 movies with the
   # largest casts (i.e most actors)
 
+  Movie.joins(:castings).select('movies.id, movies.title').group('castings.actor_id').order('COUNT(castings: {:actor_id}) DESC').limit(3)
+
 end
 
 def directed_by_one_of(them)
