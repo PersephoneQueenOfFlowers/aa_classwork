ae5396c64c2d5249e3a389282de946559882e6d8
JosephASandoval <josephasandoval17@gmail.com>
2020-12-29 15:20:55 -0800

    Made a create method

--- w6d2/routes_controllers/app/controllers/users_controller.rb
+++ w6d2/routes_controllers/app/controllers/users_controller.rb
@@ -1,6 +1,11 @@
 class UsersController < ApplicationController
   def index
-    debugger
     render plain: "I'm in the index action!"
   end
+
+  def create
+    render json: params
+  end
+
+  
 end
