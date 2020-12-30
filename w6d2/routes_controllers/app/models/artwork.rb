8ea936b6371409988c7cac9ea76ce5a6b5366d86
JosephASandoval <josephasandoval17@gmail.com>
2020-12-30 15:17:43 -0800

    Created association

--- w6d2/routes_controllers/app/models/artwork.rb
+++ w6d2/routes_controllers/app/models/artwork.rb
@@ -4,5 +4,9 @@ class Artwork < ApplicationRecord
   validates :image_url, presence: true
   validates :artist_id, presence: true
 
+  belongs_to :artist,
+    primary_key: :id,
+    foreign_key: :artist_id,
+    class_name: :User
   
 end
--- w6d2/routes_controllers/db/schema.rb
+++ w6d2/routes_controllers/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_30_224527) do
+ActiveRecord::Schema.define(version: 2020_12_30_231053) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2020_12_30_224527) do
     t.string "title", null: false
     t.string "image_url", null: false
     t.integer "artist_id", null: false
+    t.index ["artist_id"], name: "index_artworks_on_artist_id"
     t.index ["title"], name: "index_artworks_on_title"
   end
 
