3cffddf2f5dfc826977d5fc09a0406b614edb4e9
Dmitrii An <an.dmitriy93@gmail.com>
2021-01-05 05:05:10 +0600

    Using the session

--- w7d1/ninety-nine-cats-auth/app/controllers/sessions_controller.rb
+++ w7d1/ninety-nine-cats-auth/app/controllers/sessions_controller.rb
@@ -1,26 +1,29 @@
 class SessionsController < ApplicationController
     
     def new
-        render :new
+      render :new
     end
 
 
     def create
-        @user = User.find_by_credentials(
-          params[:user][:username],
-          params[:user][:password]
-        )
+      @user = User.find_by_credentials(
+        params[:user][:username],
+        params[:user][:password]
+      )
 
-        if @user 
-            redirect_to cats_url  
-        else
+      @user.reset_session_token!
+      session[:session_token] = @user.session_token
+      redirect_to cats_url
           
-        end
     end
 
     def destroy 
       # logout TODO we have to write this
       # redirect_to new_session_url 
+      if current_user
+        current_user.reset_session_token!
+        session[:session_token] = ""
+      end
     end
 
 
