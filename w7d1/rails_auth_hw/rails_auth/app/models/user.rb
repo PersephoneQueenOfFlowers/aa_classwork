2614ce3fa3ff37ce4625f2317890d110fdf9ce93
seth_schoenfeld <sethschoenfeld@gmail.com>
2021-01-04 13:36:30 -0800

    99 cats 1st commit

---
+++ w7d1/rails_auth_hw/rails_auth/app/models/user.rb
@@ -0,0 +1,55 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint           not null, primary key
+#  username        :string           not null
+#  password_digest :string           not null
+#  session_token   :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+require 'securerandom'
+require 'bcrypt'
+require 'byebug'
+
+class User < ApplicationRecord
+  validates :username, presence: true, uniqueness: true, null: false
+  validates :session_token, presence: true, null: false
+  validates_presence_of :password_digest, message: 'Password can\'t be blank' 
+  validates :password, length: { minimum: 6 }
+  before_validation :ensure_session_token
+
+  attr_reader :password
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by(username: username)
+    return nil if user.nil?
+    user.is_password?(password) ? user : nil
+  end
+
+  def generate_session_token
+    SecureRandom.base64(16)
+  end
+
+  def reset_session_token!
+    self.session_token = SecureRandom.urlsafe_base64(16)
+    self.save!
+    self.session_token
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  private
+  def ensure_session_token
+    self.session_token ||= SecureRandom.urlsafe_base64(16)
+  end
+
+end
