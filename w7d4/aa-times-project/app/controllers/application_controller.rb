9402380c40b8de2f0d427e0799e8b01f5e08110c
seth_schoenfeld <sethschoenfeld@gmail.com>
2021-01-07 14:30:16 -0800

    masthead work, weather and date more

--- w7d4/aa-times-project/app/controllers/application_controller.rb
+++ w7d4/aa-times-project/app/controllers/application_controller.rb
@@ -1,6 +1,3 @@
-
-require 'yahoo-weather'
-
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
