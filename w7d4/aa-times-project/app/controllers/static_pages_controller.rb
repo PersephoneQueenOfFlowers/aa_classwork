63b22ca32d78f68980685f3f81c6be0545d3858d
seth_schoenfeld <sethschoenfeld@gmail.com>
2021-01-07 14:26:36 -0800

    masthead work, weather and date

--- w7d4/aa-times-project/app/controllers/static_pages_controller.rb
+++ w7d4/aa-times-project/app/controllers/static_pages_controller.rb
@@ -1,4 +1,9 @@
 class StaticPagesController < ApplicationController
-	def index
+  def index
+    time = Time.new
+    @today = time.strftime("%B %d, %Y")
+
+    @client = YahooWeather::Client.new
+    @response = @client.lookup_by_woeid(2487956)
 	end
 end
