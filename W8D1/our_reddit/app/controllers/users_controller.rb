fbccb962da7d872058a4e272578def3ba6e47e76
Brandon Estano <estanob@gmail.com>
2021-01-11 10:36:44 -0800

    Build UsersController methods

--- W8D1/our_reddit/app/controllers/users_controller.rb
+++ W8D1/our_reddit/app/controllers/users_controller.rb
@@ -1,2 +1,51 @@
 class UsersController < ApplicationController
+
+  def index
+    @users = User.all
+    render :index
+  end
+
+  def create
+    @user = User.new(user_params)
+    
+    if @user.save
+      login(@user)
+      redirect_to users_url
+    else
+      flash[:errors] = @user.errors.full_messages
+      render :new
+    end
+  end
+
+  def new
+    @user = User.new
+    render :new
+  end
+
+  def edit
+
+  end
+
+  def show
+    @user = User.find(params[:id])
+    render :show
+  end
+
+  def update
+
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password)
+  end
+
 end
+
+# subs#index
+# subs#create
+# subs#new
+# subs#edit
+# subs#show
+# subs#update
