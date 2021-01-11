a0407ec58ab2fcfddec4234e0bd5a65aa8789594
Brandon Estano <estanob@gmail.com>
2021-01-11 14:32:55 -0800

    Complete new user sign up page

--- W8D1/our_reddit/app/controllers/users_controller.rb
+++ W8D1/our_reddit/app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
 
-  before_action :require_logged_in
+  before_action :require_logged_in, except: [:create, :new]
 
   def index
     @users = User.all
