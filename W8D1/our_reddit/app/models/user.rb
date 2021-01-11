c0d4613b2e5ec815dc1f51d86615ef4de44e973a
seth_schoenfeld <sethschoenfeld@gmail.com>
2021-01-11 11:25:39 -0800

    drew out associations

--- W8D1/our_reddit/app/models/user.rb
+++ W8D1/our_reddit/app/models/user.rb
@@ -1,4 +1,32 @@
 class User < ApplicationRecord
+  attr_reader :password 
+
+  validates :username, :password_digest, :session_token, presence: true
+  validates :username, uniqueness: true 
+  validates :password, length: {minimum: 6, allow_nil: true}
+
+  after_initialize :ensure_session_token 
+
+  has_many :posts, as :postable
+
+
+#   class Picture < ApplicationRecord
+#   belongs_to :imageable, polymorphic: true
+# end
+
+# class Employee < ApplicationRecord
+#   has_many :pictures, as: :imageable
+# end
+
+# class Product < ApplicationRecord
+#   has_many :pictures, as: :imageable
+# end
+
+
+
+
+
+
 
 
   def self.find_by_credentials(username, password)
