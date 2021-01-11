7a953d4ab752c6a3a2bb11b8978a18200801ac5e
Brandon Estano <estanob@gmail.com>
2021-01-11 12:20:26 -0800

    Create edit route

--- W8D1/our_reddit/config/routes.rb
+++ W8D1/our_reddit/config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :subs, except: [:destroy]
+  resources :subs, except: [:edit, :destroy]
 
-  resources :users
+  resources :users do
+    resources :subs, only: [:edit]
+  end
 
 end
