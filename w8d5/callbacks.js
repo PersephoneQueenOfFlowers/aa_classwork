85cc713db3fa5c79135bb6d780fe1eec81be9c57
seth_schoenfeld <sethschoenfeld@gmail.com>
2021-01-15 15:20:21 -0800

    clock finished, addNumbers started

--- w8d5/callbacks.js
+++ w8d5/callbacks.js
@@ -9,7 +9,6 @@ class Clock {
     this.minutes = date.getMinutes();
     this.hours = date.getHours();
     // console.log("current time => " + date)
-    console.log("this =>" + this)
     let boundTick = this._tick.bind(this)
     setInterval(boundTick, 1000)
   }
@@ -17,14 +16,30 @@ class Clock {
   printTime() {
     // Format the time in HH:MM:SS
     // Use console.log to print it.
-
+    let time = `${this.hours}:${this.minutes}:${this.seconds}`;
+    console.log("current time => " + time);
     // console.log("current time => " + date);
   }
 
   _tick() {
+    if(this.seconds === 59){
+      this.seconds = 0;
+      this.minutes += 1;
+    }else {
+      this.seconds += 1;
+    }
+
+    if (this.minutes === 59 && this.seconds === 59) {
+      this.minutes = 0;
+      this.hours += 1;
+    } 
+
+    if (this.hours === 23 && this.minutes === 59 && this.seconds === 59) {
+      this.hours = 0;
+    } 
     // 1. Increment the time by one second.
     // 2. Call printTime.
-    console.log(this)
+    this.printTime()
   }
 }
 
