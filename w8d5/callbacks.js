269bd39c677cc9676d3fbd45436772cba527b245
seth_schoenfeld <sethschoenfeld@gmail.com>
2021-01-15 13:37:07 -0800

    skeleton for exercises

--- w8d5/callbacks.js
+++ w8d5/callbacks.js
@@ -1,1 +1,20 @@
-// callback exercises here
+class Clock {
+  constructor() {
+    // 1. Create a Date object.
+    // 2. Store the hours, minutes, and seconds.
+    // 3. Call printTime.
+    // 4. Schedule the tick at 1 second intervals.
+  }
+
+  printTime() {
+    // Format the time in HH:MM:SS
+    // Use console.log to print it.
+  }
+
+  _tick() {
+    // 1. Increment the time by one second.
+    // 2. Call printTime.
+  }
+}
+
+const clock = new Clock();
