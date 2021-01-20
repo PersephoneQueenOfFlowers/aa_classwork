4aace20063ab06df1dea0a153396c6e9e3eee7aa
seth_schoenfeld <sethschoenfeld@gmail.com>
2021-01-20 09:23:36 -0800

    added skeleton for asteroids

---
+++ w9d2/asteroids/src/game.js
@@ -0,0 +1,17 @@
+// Game(src / game.js)
+// Holds collections of the asteroids, bullets, and your ship.
+//   Game.prototype.step method calls Game.prototype.move on all the objects, and Game.prototype.checkCollisions checks for colliding objects.
+//     Game.prototype.draw(ctx) draws the game.
+// Keeps track of dimensions of the space; wraps objects around when they drift off the screen.
+
+
+
+
+
+// Game.prototype.step
+
+// Game.prototype.move
+
+// Game.prototype.checkCollisions
+
+// Game.prototype.draw(ctx)
---
+++ w9d2/asteroids/src/game_view.js
@@ -0,0 +1,5 @@
+// GameView(src / game_view.js)
+// Stores a Game instance.
+// Stores a canvas context to draw the game into.
+// Installs key listeners to move the ship and fire bullets.
+// Installs a timer to call Game.prototype.step
