99815862aef82235a37f13d9faaa773fcda678f3
seth_schoenfeld <sethschoenfeld@gmail.com>
2021-01-21 14:36:02 -0800

    wrote ajax for follow button

--- w9d4/ajax-twitter/frontend/follow_toggle.js
+++ w9d4/ajax-twitter/frontend/follow_toggle.js
@@ -16,6 +16,29 @@ class FollowToggle {
     } else if (this.followState === "followed")
       $('.follow-toggle').text('Unfollow!');
   }
+
+  handleClick(e){
+    e.preventDefault();
+    e.stopPropagation();
+    debugger;
+    if (this.followState === "unfollowed") {
+      // ajax post request
+      $.ajax({
+        type: 'POST',
+        url: "/users/${this.userid}/follow"
+      });
+      this.followState = "followed";
+    } else if (this.followState === "followed"){
+      // ajax delete request
+      $.ajax({
+        type: 'DELETE',
+        url: "/users/${this.userid}/follow"
+      });
+      this.followState = "unfollowed";
+    }
+    this.render();
+  }
 }
 
 module.exports = FollowToggle;
+
