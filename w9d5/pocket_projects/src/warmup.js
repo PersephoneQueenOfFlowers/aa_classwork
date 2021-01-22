e4f7c25857d0de1fa281394df2ef525093f9b432
Michelle Ha <michelle1992us@yahoo.com>
2021-01-22 14:58:00 -0800

    started warmup

--- w9d5/pocket_projects/src/warmup.js
+++ w9d5/pocket_projects/src/warmup.js
@@ -1,8 +1,13 @@
 
 const partyHeader = document.getElementById('party');
+const welcome = document.getElementsByClassName('welcome');
+console.log(welcome)
 
 export const htmlGenerator = (string, htmlElement) => {
-
+    let element = document.createElement("p");
+    const newContent = document.createTextNode(string);
+    element.appendChild(newContent);
+    welcome.insertBefore(element, partyHeader.nextSibling);
 };
 
 htmlGenerator('Party Time.', partyHeader);
