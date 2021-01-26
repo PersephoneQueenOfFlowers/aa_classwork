277b26cb4dc015c371ddd99efd97c3df6095197f
seth_schoenfeld <sethschoenfeld@gmail.com>
2021-01-26 15:05:30 -0800

    bugs in jsx

--- w10d2/widgettes/frontend/clock.jsx
+++ w10d2/widgettes/frontend/clock.jsx
@@ -1,20 +1,21 @@
-
-class Clock {
-  constructor() {
-    this.time = new Date();
+import React from 'react';
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {time: new Date()};
+    setInterval(this._tick.bind(this), 1000);
   }
 
   _tick() {
-    this.setState({  })
+    this.setState({ time: new Date() });
   }
-
-  setInterval( function() {
-       _tick(); 
-      }, 1000);
 
   render() {
     return (
-       <h1>Clock</h1>
+      <div>
+        <h1>Clock</h1>
+        <h2 className="time">{this.state.time}</h2>
+      </div>
       );
   }
 }
--- w10d2/widgettes/frontend/widgettes.jsx
+++ w10d2/widgettes/frontend/widgettes.jsx
@@ -1,8 +1,25 @@
 import React    from 'react';
 import ReactDOM from 'react-dom';
+import Clock from './clock';
+
+class Widgette extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>hello world from widgettes</p>
+        <Clock />
+        </div>
+    );
+  }
+}
 
 document.addEventListener('DOMContentLoaded', () => {
   const reactRoot = document.getElementById('root');
+    console.log("hello world");
   // ReactDOM.render(<h1>Our first piece of React!!!!!!!</h1>,reactRoot);
   ReactDOM.render(<Widgette />, reactRoot);
 });
+
+
+
+
