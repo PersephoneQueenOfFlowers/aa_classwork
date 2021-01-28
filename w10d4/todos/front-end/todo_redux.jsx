90b2ac2992f220fb693a4f1c09a1de14c2f683ed
seth_schoenfeld <sethschoenfeld@gmail.com>
2021-01-28 13:59:06 -0800

    started project

---
+++ w10d4/todos/front-end/todo_redux.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import configureStore from './store/store'; // exported default 
+
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("Welcome to the todos");
+  const reactRoot = document.getElementById('root');
+  window.store = configureStore();
+
+  ReactDOM.render(<h1>welcome to todo app</h1>, reactRoot);
+})
