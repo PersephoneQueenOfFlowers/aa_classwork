b24d15a0f47b1fb3fe93a4e28d4aec16ed85bad9
seth_schoenfeld <sethschoenfeld@gmail.com>
2021-02-02 15:10:29 -0800

    did another few specs

--- w11d2/jBuilder/app/controllers/api/guests_controller.rb
+++ w11d2/jBuilder/app/controllers/api/guests_controller.rb
@@ -1,15 +1,12 @@
 class Api::GuestsController < ApplicationController
   def index
     @guests = Guest.all
+    # @gifts = Gift.find(:id)
     render :index
   end
 
   def show
-    @guest = Guest.find_by(id: params[:id])
-    # g = {}
-    # g[:age] = @guest.age
-    # g[:name] = @guest.name
-    # g[:favorite_color] = @guest.favorite_color    
+    @guest = Guest.find_by(id: params[:id])   
     render :show
     #passed spec, now rendering too much info
     # render json: g
--- w11d2/jBuilder/app/views/api/guests/index.json.jbuilder
+++ w11d2/jBuilder/app/views/api/guests/index.json.jbuilder
@@ -1,3 +1,10 @@
 json.array! @guests do |guest|
-    json.name guest.name
+  json.name guest.name
+
 end
+
+# json.array! @gifts do |gift|
+#   if gift.guest_id === guest.id 
+#     json.title gift.title 
+#   end
+# end
