36dd56057fc2d11034fa4d6e9a893b2683e4b8e3
seth_schoenfeld <sethschoenfeld@gmail.com>
2021-02-02 12:31:50 -0800

    added static_pages_controller

---
+++ w11d2/jBuilder/app/views/api/guests/show.json.jbuilder
@@ -0,0 +1,3 @@
+json.guests do
+    json.extract! @guest, :name, :age, :favorite_color
+end
--- w11d2/jBuilder/config/routes.rb
+++ w11d2/jBuilder/config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  # Your routes here!
+  namespace :api, defaults: { format: :json } do
+    resources :gifts, only: [ :show, :index ]
+    resources :guests, only: [ :show, :index ]
+    resources :parties, only: [ :show, :index ]
+  end
 
+  root to: "static_pages#root"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
