9bc0b993efa55f63524757d5cfa21ce7f30c1e9b
seth_schoenfeld <sethschoenfeld@gmail.com>
2021-02-04 14:55:29 -0800

    wrote out crlll, spire, user model validations

---
+++ w11d4/benchBnB/app/models/user.rb
@@ -0,0 +1,33 @@
+class User < ApplicationRecord
+
+  attr_reader :password 
+  validates :username, :session_token, presence: true, uniqueness: true
+  validates :password, length: { mininum: 6, allow_nil: true}
+  after_initialize :ensure_session_token
+
+  def self.find_by_credentials(username, password) 
+    user = User.find_by(username: username)
+
+    user && user.is_password?(password) ? (user) : (nil)
+  end
+
+  def password=(pw)
+    self.password_digest = BCrypt::Password.create(pw)
+    @password = pw
+  end
+
+  def is_password?(pw)
+    BCrypt::Password.new(self.password_digest).is_password?(pw)
+  end
+
+  def reset_session_token!
+    self.session_token = SecureRandom.Base64
+    self.save! 
+    self.session_token 
+  end
+
+  def ensure_session_token
+    self.session_token ||= SecureRandom.Base64 
+  end
+
+end
